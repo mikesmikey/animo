bfbb39c48e360b8b75ef4d44a85c52fd87aab94e
Kraiwich <le_kraiwich@hotmail.com>
2024-04-26 18:32:33 +0700

    Change chapter id in a chapter model to use uuid format

--- src/models/chapter.model.ts
+++ src/models/chapter.model.ts
@@ -1,13 +1,18 @@
 import {Entity, model, property, belongsTo} from '@loopback/repository';
 import {Anime} from './anime.model';
 import {Studio} from './studio.model';
+import uuid from 'uuid';
 
 @model()
 export class Chapter extends Entity {
   @property({
     type: 'string',
     id: true,
-    generated: true,
+    defaultFn: "uuid",
+    validator: (value: string) => {
+      if (!uuid.validate(value))
+        throw new Error('Invalid id format (must be in UUID format)')
+    }
   })
   id?: string;
 

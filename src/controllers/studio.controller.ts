c5697395a6f9f2aac5bcaae9ed483680113d20ac
Kraiwich <le_kraiwich@hotmail.com>
2024-04-26 17:34:10 +0700

    Create endpoints for a studio

---
+++ src/controllers/studio.controller.ts
@@ -0,0 +1,150 @@
+import {
+  Count,
+  CountSchema,
+  Filter,
+  FilterExcludingWhere,
+  repository,
+  Where,
+} from '@loopback/repository';
+import {
+  post,
+  param,
+  get,
+  getModelSchemaRef,
+  patch,
+  put,
+  del,
+  requestBody,
+  response,
+} from '@loopback/rest';
+import {Studio} from '../models';
+import {StudioRepository} from '../repositories';
+
+export class StudioController {
+  constructor(
+    @repository(StudioRepository)
+    public studioRepository : StudioRepository,
+  ) {}
+
+  @post('/studio')
+  @response(200, {
+    description: 'Studio model instance',
+    content: {'application/json': {schema: getModelSchemaRef(Studio)}},
+  })
+  async create(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Studio, {
+            title: 'NewStudio',
+            
+          }),
+        },
+      },
+    })
+    studio: Studio,
+  ): Promise<Studio> {
+    return this.studioRepository.create(studio);
+  }
+
+  @get('/studio/count')
+  @response(200, {
+    description: 'Studio model count',
+    content: {'application/json': {schema: CountSchema}},
+  })
+  async count(
+    @param.where(Studio) where?: Where<Studio>,
+  ): Promise<Count> {
+    return this.studioRepository.count(where);
+  }
+
+  @get('/studio')
+  @response(200, {
+    description: 'Array of Studio model instances',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'array',
+          items: getModelSchemaRef(Studio, {includeRelations: true}),
+        },
+      },
+    },
+  })
+  async find(
+    @param.filter(Studio) filter?: Filter<Studio>,
+  ): Promise<Studio[]> {
+    return this.studioRepository.find(filter);
+  }
+
+  @patch('/studio')
+  @response(200, {
+    description: 'Studio PATCH success count',
+    content: {'application/json': {schema: CountSchema}},
+  })
+  async updateAll(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Studio, {partial: true}),
+        },
+      },
+    })
+    studio: Studio,
+    @param.where(Studio) where?: Where<Studio>,
+  ): Promise<Count> {
+    return this.studioRepository.updateAll(studio, where);
+  }
+
+  @get('/studio/{id}')
+  @response(200, {
+    description: 'Studio model instance',
+    content: {
+      'application/json': {
+        schema: getModelSchemaRef(Studio, {includeRelations: true}),
+      },
+    },
+  })
+  async findById(
+    @param.path.string('id') id: string,
+    @param.filter(Studio, {exclude: 'where'}) filter?: FilterExcludingWhere<Studio>
+  ): Promise<Studio> {
+    return this.studioRepository.findById(id, filter);
+  }
+
+  @patch('/studio/{id}')
+  @response(204, {
+    description: 'Studio PATCH success',
+  })
+  async updateById(
+    @param.path.string('id') id: string,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Studio, {partial: true}),
+        },
+      },
+    })
+    studio: Studio,
+  ): Promise<void> {
+    await this.studioRepository.updateById(id, studio);
+  }
+
+  @put('/studio/{id}')
+  @response(204, {
+    description: 'Studio PUT success',
+  })
+  async replaceById(
+    @param.path.string('id') id: string,
+    @requestBody() studio: Studio,
+  ): Promise<void> {
+    await this.studioRepository.replaceById(id, studio);
+  }
+
+  @del('/studio/{id}')
+  @response(204, {
+    description: 'Studio DELETE success',
+  })
+  async deleteById(@param.path.string('id') id: string): Promise<void> {
+    await this.studioRepository.deleteById(id);
+  }
+}
